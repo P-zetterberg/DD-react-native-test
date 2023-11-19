3e3a7363209066ac8b2ba3219b56d269e78051fb
Pontus <pontus.z94@gmail.com>
2023-11-19 14:46:40 +0100

    async storage

--- src/contexts/ListContext.tsx
+++ src/contexts/ListContext.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, useState, useContext, ReactNode } from 'react';
+import React, { createContext, useState, useContext, ReactNode, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 interface Item {
   [key: string]: string | number;
@@ -22,13 +23,18 @@ const ListProvider: React.FC<ListProviderProps> = ({ children }) => {
   const [list, setList] = useState<Item[]>([]);
 
   const addItemToList = (item: Item) => {
-    setList((prevList) => [...prevList, item]);
+    setList((prevList) => {
+      storeData([...prevList, item])
+     return [...prevList, item]
+    });
+    
   };
 
   const deleteItemFromList = (index:number) => {
     setList((prevList) => {
       const newList = [...prevList];
       newList.splice(index, 1);
+      storeData(newList)
       return newList;
     });
   };
@@ -38,9 +44,30 @@ const ListProvider: React.FC<ListProviderProps> = ({ children }) => {
       if (newList[index]) {
         newList[index] = updatedItem;
       }
+      storeData(newList)
       return newList;
     });
   };
+  const storeData = async (value:Item[]) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem('array', jsonValue);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const getData = async () => {
+    try {
+      const jsonValue  = await AsyncStorage.getItem('array');
+      setList(jsonValue != null ? JSON.parse(jsonValue) : [])
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    getData()
+  } , [])
+  
   return (
     <ListContext.Provider value={{ list, addItemToList, updateItemInList, deleteItemFromList }}>
       {children}
@@ -48,8 +75,10 @@ const ListProvider: React.FC<ListProviderProps> = ({ children }) => {
   );
 };
 
+
 const useList = (): ListContextType => {
   const context = useContext(ListContext);
+
   if (!context) {
     throw new Error('useList must be used within a ListProvider');
   }
